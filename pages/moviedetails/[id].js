eb05e32d583de10e9e52ac996b1f6a332ba72ccf
root-moody <moodyamin531@gmail.com>
2022-12-13 09:47:38 +0300

    loading anime

--- pages/moviedetails/[id].js
+++ pages/moviedetails/[id].js
@@ -128,7 +128,10 @@ const MovieDetails = ({ data, image, movtrailer, id }) => {
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                                         </svg>
                                         <span>Download</span> 
-                                        <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg>     
+                                        {
+                                            loading &&
+                                           <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>     
+                                        }  
                                     </button>
                                 </Link>    
                                 <button className="text-base text-slate-200 font-bold bg-btn-blue rounded py-1 w-full flex justify-center mt-2 ">
