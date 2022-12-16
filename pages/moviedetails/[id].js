0ca725a3eeca844456e7f187322a7d3ca16c3a49
root-moody <moodyamin531@gmail.com>
2022-12-16 09:42:34 +0300

    lets see

--- pages/moviedetails/[id].js
+++ pages/moviedetails/[id].js
@@ -34,6 +34,8 @@ const MovieDetails = ({ data, image, movtrailer, id }) => {
     const [error, seterror] = useState('')
     const [size, setsize] = useState('')
     const [added, setadded] = useState(false)
+    const [load, setload] = useState(false)
+    const [download, setdownload] = useState(true)
     const {dispatch} = listcontexthook()
 
     const title = thumb.title;
@@ -49,6 +51,11 @@ const MovieDetails = ({ data, image, movtrailer, id }) => {
                 ]
             }                           
         }
+    }
+
+    function runloading() {
+        setload(true);
+        setdownload(false);
     }
 
     const key = gettrailer()
@@ -121,16 +128,19 @@ const MovieDetails = ({ data, image, movtrailer, id }) => {
                                 <img src={`${base_url}${thumb.thumb}`} className="thumbimg"/>
                             </div>
                             <div className="my-2 thumbcontainer ">
-                                <Link href={`${process.env.NEXT_PUBLIC_MOVIE_LINK}${id}`}>
-                                        <button className="text-base text-slate-200 font-bold bg-our-green rounded py-1 w-full flex justify-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                                        </svg>
-                                        <span>Download</span> 
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="animate-spin font-bold w-6 h-6">
-                                            <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
-                                        </svg>
-   
+                                <Link href={`${process.env.NEXT_PUBLIC_MOVIE_LINK}${id}`} onClick={runloading}>
+                                    <button className="text-base text-slate-200 font-bold bg-our-green rounded py-1 w-full flex justify-center">
+                                        {
+                                            download &&
+                                               
+                                                <span>Download</span>
+                                        }
+                                        {
+                                            load && 
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="animate-spin font-bold w-6 h-6">
+                                                <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
+                                            </svg>
+                                        }
                                     </button>
                                 </Link>    
                                 <button className="text-base text-slate-200 font-bold bg-btn-blue rounded py-1 w-full flex justify-center mt-2 ">
