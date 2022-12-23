c6b6a1b31c6ab8d2291d98c2a287db39d4e323c8
root-moody <moodyamin531@gmail.com>
2022-12-23 18:00:24 +0300

    tv shows

--- pages/moviedetails/tvshows.js
+++ pages/moviedetails/tvshows.js
@@ -3,7 +3,15 @@ import React from 'react'
 const tvshows = () => {
   return (
     <>
-          
+      <div className='bg-slate-900 h-fit pb-4'>
+        <div className='flex justify-center'>
+          <h1 className="heading1 sm:text-xl">With Popcorn Movies Tv Shows Are Happening</h1>
+          <p className="paragraph text-slate-100">
+            Sometimes scars you can not see are the ones that hurt the most.
+            <cite>Samantha Larusso</cite>
+          </p>
+        </div>
+      </div>   
     </>
   )
 }
