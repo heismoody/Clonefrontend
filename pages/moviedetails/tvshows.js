63c5956399aa08580c36f21b5d18444e2ccdbbc3
root-moody <moodyamin531@gmail.com>
2022-12-24 09:49:20 +0300

    today comit

--- pages/moviedetails/tvshows.js
+++ pages/moviedetails/tvshows.js
@@ -7,11 +7,14 @@ const tvshows = () => {
         <div className='flex justify-center'>
           <div>
             <h1 className="heading1 sm:text-xl">With Popcorn Movies Tv Shows Are Happening</h1>
-            <p className="paragraph text-slate-100">
+            <p className="paragraph text-center text-slate-100">
               Sometimes scars you can not see are the ones that hurt the most.
               <cite className='block'>~Samantha Larusso</cite>
             </p>
           </div>
+        </div>
+        <div>
+          <p className='heading2'>Popular Tv Shows</p>
         </div>
       </div>   
     </>
