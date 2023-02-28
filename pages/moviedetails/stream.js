5d307e428d5a77fe8a909d68f42af8f99162e3c4
root-moody <moodyamin531@gmail.com>
2023-02-28 10:32:06 +0300

    redeploy

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useRouter } from "next/router";
 
-const stream = () => {
+
+function stream() {
     const router = useRouter();
     const query = router.query;
   return (
-      <>
-          <div>Tsupp man this is your movie id {query.id} </div>
-      </>
+    <div>Tsupp man this is your movie id {query.id} </div>
   )
 }
 
