eca16e10e5f2f61176ba8e22ae6513a995014701
root-moody <moodyamin531@gmail.com>
2023-02-28 10:49:59 +0300

    mhh

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 const Stream = () => {
-    const router = useRouter();
-    const query = router.query;
-    console.log(query);
+    // const router = useRouter();
+    // const query = router.query;
+    // console.log(query);
   return (
       <>
           <div>Tsupp man this is your movie id</div>
