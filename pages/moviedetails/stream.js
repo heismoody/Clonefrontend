56d80b752b65272098444eb8060fc2b05435c523
root-moody <moodyamin531@gmail.com>
2023-02-28 10:44:11 +0300

    console

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 const Stream = () => {
     const router = useRouter();
     const query = router.query;
-    const movieid = query.id;
+    console.log(query);
   return (
       <>
           <div>Tsupp man this is your movie id {movieid}</div>
