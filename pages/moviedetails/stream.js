b47ad359ff12fac162a07f2934655502bd24d23e
root-moody <moodyamin531@gmail.com>
2023-03-01 17:18:58 +0300

    width edit

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -7,9 +7,9 @@ const Stream = () => {
     const streamlink = `https://www.2embed.to/embed/tmdb/movie?id=`
   return (
     <>
-        <div className='bg-slate-900 h-fit py-4'>
+        <div className='bg-slate-900 py-4'>
             <div className='flex justify-center'>
-                <div className="w-10/12 h-9/12 bg-black sm:youtube">
+                <div className="w-10/12 h-[80vh] bg-black sm:youtube">
                     <iframe className="thumbimg hover:opacity-70" src={`${streamlink}${query.id}`} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
                 </div>
             </div> 
