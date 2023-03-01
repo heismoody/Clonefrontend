68b570f518cd130df1e0b607d748da871f66288f
root-moody <moodyamin531@gmail.com>
2023-03-01 15:56:01 +0300

    we try streaming

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -4,9 +4,14 @@ import { useRouter } from "next/router"
 const Stream = () => {
     const router = useRouter();
     const query = router.query;
+    const streamlink = `https://autoembed.to/movie/tmdb/`
   return (
       <>
-          <div>Tsupp man this is your movie id</div>
+        <div className='bg-slate-900 h-fit pb-4'>
+            <div className="w-[600px] h-[416px] bg-black sm:youtube">
+                  <iframe className="thumbimg hover:opacity-70" src={`${streamlink}${query.id}`} width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+            </div> 
+        </div>
       </>
   )
 }
