5dcba3d1abd4f7befb78f037f5bd53a85678708d
root-moody <moodyamin531@gmail.com>
2023-03-01 16:53:37 +0300

    edits

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -9,7 +9,7 @@ const Stream = () => {
     <>
         <div className='bg-slate-900 h-fit pb-4'>
             <div className='flex justify-center'>
-                <div className="w-[800px] h-[616px] bg-black sm:youtube">
+                <div className="w-[800px] h-[600px] bg-black sm:youtube">
                     <iframe className="thumbimg hover:opacity-70" src={`${streamlink}${query.id}`} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
                 </div>
             </div> 
