5237c8bbda9f8e782ed12cf7bc8829eaad9eeb5b
root-moody <moodyamin531@gmail.com>
2023-03-03 08:52:15 +0300

    little edit

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -7,11 +7,13 @@ const Stream = () => {
     const streamlink = `https://www.2embed.to/embed/tmdb/movie?id=`
   return (
     <>
-        <div className='bg-slate-900 h-screen py-4'>
-              <div className='flex justify-center'>
+          <div className='bg-slate-900 h-screen py-4'>
+              <div>
                   <p className="paragraph">We hate ads as you do. For this might take a while for we to fix.<br></br>
                       At the moment you can use the back key to navigate back the site and continue streaming.<br></br>
                       While on computer you can close the redirecting tab</p>
+              </div>
+              <div className='flex justify-center'>
                 <div className="w-10/12 h-[80vh] bg-black sm:youtube">
                     <iframe className="thumbimg hover:opacity-70" src={`${streamlink}${query.id}`} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
                 </div>
