d6359fa7666ca69f1e457ceb812075d27c7c9e6d
root-moody <moodyamin531@gmail.com>
2023-03-04 19:27:57 +0300

    boldness

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -10,8 +10,8 @@ const Stream = () => {
           <div className='bg-slate-900 h-screen py-4'>
               <div>
                   <p className="paragraph">We hate ads as you do. For this might take a while for we to fix.
-                      At the moment you can use the back key to navigate back the site and continue streaming.
-                      While on computer you can close the redirecting tab</p>
+                      At the moment <b>you can use the back button on your phone</b> to navigate back the site and continue streaming.
+                      While on computer <b>you can close the redirecting tab</b></p>
               </div>
               <div className='flex justify-center'>
                 <div className="w-10/12 h-[80vh] bg-black sm:youtube">
