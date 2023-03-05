d9f2686e9d6c8e324ddab47fc47bf11dc66922ac
root-moody <moodyamin531@gmail.com>
2023-03-05 11:07:23 +0300

    lets test

--- pages/moviedetails/[id].js
+++ pages/moviedetails/[id].js
@@ -131,7 +131,7 @@ const MovieDetails = ({ data, details, image, id, mediatype }) => {
                                         <h4 className="detailhead">{thumb.year}</h4>
                                         {
                                             mediatype == "tv" &&
-                                            <h4 className="detailhead w-full sm:text-xs">{
+                                            <h4 className="detailhead bg-red-900 w-full sm:text-xs">{
                                                     seasons.map((season, index) => {
                                                         if (index <= number_of_seasons) {
                                                             return <div className = "grid grid-cols-4 gap-3"><div className="border border-slate-300 rounded text-our-green px-2 mx-[2px] text-sm ">Season {season.season_number}</div></div>
@@ -191,7 +191,7 @@ const MovieDetails = ({ data, details, image, id, mediatype }) => {
                                         <h4 className="detailhead sm:text-xs">{thumb.year}</h4>
                                         {
                                             mediatype == "tv" &&
-                                            <h4 className="detailhead w-full sm:text-xs">{
+                                            <h4 className="detailhead bg-red-900 sm:text-xs">{
                                                 seasons.map((season, index) => {
                                                     if (index <= number_of_seasons) {
                                                         return <div className = "grid grid-cols-4 gap-3 w-full"><div className="border border-slate-300 rounded text-our-green px-2 mx-[2px] text-sm ">Season {season.season_number}</div></div>
