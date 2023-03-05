e4d8a636671cbccdd1f33d6a0aff59cac7ac43ee
root-moody <moodyamin531@gmail.com>
2023-03-05 17:56:11 +0300

    tvepisodes

--- pages/tvdetails/[id].js
+++ pages/tvdetails/[id].js
@@ -1,30 +1,29 @@
 import React from 'react';
 
-// export async function bringepisodes() {
-//     const id = context.query.id;
-//     const seasonid = context.query.movieid;
-//     const res = await fetch(`https://api.themoviedb.org/3/tv/${seasonid}/season/${id}?api_key=${process.env.API_KEY}`);
-//     const data = await res.json();
+export async function getServerSideProps(context) {
+    const id = context.query.id;
+    const seasonid = context.query.movieid;
+    const res = await fetch(`https://api.themoviedb.org/3/tv/${seasonid}/season/${id}?api_key=${process.env.API_KEY}`);
+    const data = await res.json();
 
-//     return {
-//         props: {
-//             data
-//         }
-//     }
-// }
+    return {
+        props: {
+            data
+        }
+    }
+}
 
-const Tvdetails = ({ }) => {
-    // const { episodes } = data;
+const Tvdetails = ({ data}) => {
+    const { episodes } = data;
   return (
       <>
-        {/* {
+        {
             episodes.map((episode, index) => {
                 if (index <= 2) {
                      return <div>{episode.name}</div>
                 }
             })      
-        } */}
-          <div>here we are</div>
+        }
       </>
   )
 }
