673b08b8f091e02976cb8dc3f37e9cf4fd0ef4ff
root-moody <moodyamin531@gmail.com>
2023-03-05 18:49:40 +0300

    hows the progress

--- pages/tvdetails/[id].js
+++ pages/tvdetails/[id].js
@@ -1,4 +1,6 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+
 
 export async function getServerSideProps(context) {
     const id = context.query.id;
@@ -13,17 +15,30 @@ export async function getServerSideProps(context) {
     }
 }
 
-const Tvdetails = ({ data}) => {
+const Tvdetails = ({ data }) => {
+    const base_url = 'https://image.tmdb.org/t/p/original';
     const { episodes } = data;
+    const router = useRouter();
+    const data = router.query;
   return (
       <>
+        <div className='flex justify-center bg-slate-900 py-3'>
+            <div className='flex'>
+                <div className="thumbcont h-[138px] w-[92px] border-[2px] m-1 md:thumbcontmobile">
+                      <img src={`${base_url}${data.img}`} className="thumbimg" />
+                  </div>
+                  <div>
+                      <span>{data.seasonname}{data.seasonyear}</span>
+                  </div>
+            </div>
         {
             episodes.map((episode, index) => {
                 if (index <= 2) {
                      return <div>{episode.name}</div>
                 }
             })      
-        }
+        }   
+        </div>
       </>
   )
 }
