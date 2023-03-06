8de9d4d255020370235ad5cf6c92945b4dd54720
root-moody <moodyamin531@gmail.com>
2023-03-06 22:18:46 +0300

    tv shows change

--- pages/moviedetails/stream.js
+++ pages/moviedetails/stream.js
@@ -4,15 +4,19 @@ import { useRouter } from "next/router"
 const Stream = () => {
     const router = useRouter();
     const query = router.query;
-    const streamlink = `https://www.2embed.to/embed/tmdb/movie?id=`
-    const streamtvlink = `https://www.2embed.to/embed/tmdb/tv?id=`
+    const streamlink = `https://autoembed.to/movie/tmdb/`
+    const streamtvlink = `https://autoembed.to/tv/tmdb/`
   return (
     <>
           <div className='bg-slate-900 h-screen py-4'>
               <div className='w-full flex justify-center'>
+                <div>
+                  <h6 className='paragraph'>TRY USING ANOTHER SERVER IF THE CURRENT ONE IS NOT RESPONDING</h6>
                   <p className="paragraph">We hate ads as you do. For this might take a while for we to fix.
                       At the moment <b>you can use the back button on your phone</b> to navigate back the site and continue streaming.
-                      While on computer <b>you can close the redirecting tab</b></p>
+                      While on computer <b>you can close the redirecting tab</b>
+                  </p>
+                </div>
               </div>
               <div className='flex justify-center'>
                 <div className="w-10/12 h-[80vh] bg-black sm:youtube">
@@ -22,7 +26,7 @@ const Stream = () => {
                   }
                   {
                     query.mediatype == "tv" &&
-                    <iframe className="thumbimg" src={`${streamtvlink}${query.id}&s=${query.seasonnum}&e=${query.episodenum}`} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
+                    <iframe className="thumbimg" src={`${streamtvlink}${query.id}-${query.seasonnum}-${query.episodenum}`} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
                   }
                 </div>
             </div> 
