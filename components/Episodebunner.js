b17fbeffe35f5def2bdba42c52d4d941e65ec0e9
root-moody <moodyamin531@gmail.com>
2023-03-06 15:36:29 +0300

    here we go

--- components/Episodebunner.js
+++ components/Episodebunner.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Episodebunner = ({episodenumber, episodename, episodedate, overview, episodeimg, runtime}) => {
     const base_url = 'https://image.tmdb.org/t/p/original';
     return (
-    <div className='flex'>
-        <div className='w-[150px] h-full'>
+    <div className='flex sm:epsdcont'>
+        <div className='w-[150px] h-full sm:epsdcontimg'>
             <img src={`${base_url}${episodeimg}`} className="thumbimg" />
         </div>
         <div className='flex flex-col justify-around'>
             <div>
-                <h2>{episodenumber} {episodename}</h2>
-                <h6>Release Date: {episodedate}</h6>
-                <h6>Duration: {runtime} mins</h6>    
+                <h2 className="text-slate-100 font-bold text-base sm:text-sm">{episodenumber} {episodename}</h2>
+                <h6 className="text-slate-100 font-bold text-base sm:text-sm">Release Date: {episodedate}</h6>
+                <h6 className="text-slate-100 font-bold text-base sm:text-sm">Duration: {runtime} mins</h6>    
             </div>
             <div>
                 <p className="paragraph text-left max-w-lg sm:px-3 w-full">{overview}</p>     
