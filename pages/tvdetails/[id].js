6f0013f5a342fbfab9555e9e78682025a51e9933
root-moody <moodyamin531@gmail.com>
2023-03-07 16:54:38 +0300

    trying

--- pages/tvdetails/[id].js
+++ pages/tvdetails/[id].js
@@ -27,7 +27,7 @@ const Tvdetails = ({ data, seasonid, id }) => {
     <>
         <div className='flex justify-center bg-slate-900 py-3'>
             <div className='w-full'>
-                <div className='flex'>
+                <div className='flex bg-orange-500'>
                     <div className="h-[150px] w-[125px] px-4 pb-3 md:epsdcontmobile">
                         <img src={`${base_url}${seasondata.img}`} className="thumbimg" />
                     </div>
