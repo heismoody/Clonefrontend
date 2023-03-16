dd1e71876afcb07264e88fcb34ceebc58350b5b3
root-moody <moodyamin531@gmail.com>
2023-03-16 09:54:58 +0300

    we try this

--- pages/moviedetails/tvshows.js
+++ pages/moviedetails/tvshows.js
@@ -19,15 +19,22 @@ const tvshows = ({ data }) => {
       <div className='bg-slate-900 h-fit pb-4'>
         <div className='flex justify-center'>
           <div>
-            <h1 className="heading1 sm:text-xl">With Popcorn Movies Tv Shows Are Happening</h1>
-            <p className="paragraph text-center w-full text-slate-100">
+            <h1 className="heading1 sm:text-xl">With Popcorn Movies: Tv Shows Are Happening</h1>
+            <p className="paragraph w-full text-slate-100 bg-orange-500">
               Sometimes scars you can not see are the ones that hurt the most.
               <cite className='block'>~Samantha Larusso</cite>
             </p>
           </div>
         </div>
-        <div>
-          <p className='heading2 w-full'>Popular Tv Shows</p>
+        <div className="flex justify-center mt-0 pt-0">
+          <div className="flex items-center w-fit">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-our-green text-our-green" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+            </span>
+            <span className="heading2 pl-2">Popular Downloads</span>
+          </div>
         </div>
         <div className="flex justify-center">
           <div className="w-[1100px] flex justify-between px-5 sm:thumbmob">
